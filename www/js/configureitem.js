d90922e339e9b04726b2565127771d4d1fff4c19
David Blumenthal <github@blumenthal.com>
2013-07-16 22:30:41 -0500

    Added displayCategories

--- www/js/configureitem.js
+++ www/js/configureitem.js
@@ -55,7 +55,69 @@ function configureItem() {
 }
 
 
+
+function displayCategories() {
+	/*         <div  data-role="fieldcontain">
+            <fieldset data-role="controlgroup">
+                <legend>Choose which categories this applies to:</legend>
+                <input type="checkbox" name="checkbox-1a" id="checkbox-1a" class="custom" />
+                <label for="checkbox-1a">Central Market</label>
+                <input type="checkbox" name="checkbox-1b" id="checkbox-1b" class="custom" />
+                <label for="checkbox-1b">Breed & Co</label>
+            </fieldset>
+        </div>
+        */
+        console.log("displayCategories - top\n");
+		
+	var listName = getUrlVars()["list"];
+		
+	//Note, this assumes listName is a valid list
+	var data = get_data(),
+		categories = data[listName]["categories"],
+		crossedOffItems = data[listName]["crossedOffItems"],
+		fieldContainElem, fieldSetElem, inputElem, labelElem;
+	
+	if (categories === undefined) {
+		categories = [];
+	}
+		
+	if (categories.length == 0) {
+		console.log("No items found for " + listName);
+		$("#categories").html("Click 'Configure Categories' to add a category");
+	}
+	else {
+		console.log(categories.length + " categories found");
+		fieldContainElem = $("<div>", {"data-role":"fieldcontain"});
+		fieldSetElem = $("<fieldset>", {"data-role":"controlgroup"});
+		fieldSetElem.append("<legend>Choose which categories this applies to:</legend>");
+
+		$.each(categories, function (index, value) {
+			console.log("   Adding " + value["name"]);;
+			inputElem = $("<input>", {"type":"checkbox", class:"custom", "id":"checkbox-"+index});
+			fieldSetElem.append(inputElem);
+			labelElem = $("<label>", {"for":"checkbox-"+index});
+			labelElem.text(value["name"]);
+			fieldSetElem.append(labelElem);
+	    	});//each item
+	    	fieldContainElem.append(fieldSetElem);
+		//replace the current lists div contents with the new unordered list
+	    	$("#categories").html(fieldContainElem);
+console.log("New html = "+ $("#categories").html());
+	    
+	    	//have to explicitly transform to pretty view after initial page load
+	    	$("#categories").trigger('create');
+	}
+}//displayCategories
+
+
 $(document).on('click', '#saveAddItem', function() {
 	console.log("Clicked on saveAddItem");
 	configureItem();
 });
+
+
+$(document).on('pageshow', '#config-item-dialog', function() {
+	var listName = getUrlVars()["list"];
+	displayCategories(listName);
+});
+
