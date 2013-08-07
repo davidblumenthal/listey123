ed53b2640ec89e8ef8c14bd36cbe7eb771afbb40
David Blumenthal <github@blumenthal.com>
2013-08-06 19:05:50 -0500

    converted to use list accessor methods

--- www/js/listpage.js
+++ www/js/listpage.js
@@ -1,41 +1,40 @@
 
 function displayLists() {
-	var data = get_data(),
-		listNames = keys(data).sort(),
-		ulElem,
-		liElem,
-		aElem,
-		itemsList,
-		numItems;
+    var listNames = getListNames(),
+        ulElem,
+        liElem,
+        aElem,
+        itemsList,
+        numItems;
 
-	if (listNames.length == 0) {
-		$("#lists").html("Click 'Add A List' to create a list");
-	}
-	else {
-		ulElem = $("<ul>", {"data-role":"listview", "data-count-theme":"c", "data-inset":"true"});
+    if (listNames.length == 0) {
+        $("#lists").html("Click 'Add A List' to create a list");
+    }
+    else {
+        ulElem = $("<ul>", {"data-role":"listview", "data-count-theme":"c", "data-inset":"true"});
 
-		$.each(listNames, function (index, value) {
-			liElem = $("<li>");
-			ulElem.append(liElem);
-			itemsList = data[value]["items"];
-			numItems = (itemsList === undefined) ? 0 : keys(itemsList).length;
-			aElem = $("<a href='items.html?list="+encodeURIComponent(value)+"'>" + escapeHTML(value) + "<span class='ui-li-count'>" + numItems + "</span></a>");
-			liElem.append(aElem);
-			aElem.click(function () {
-			    console.log("displayLists: clicked on " + value);
-			    //displayItems(value);
-			    return true;
-			});//aElem.click
-		});//each
+        $.each(listNames, function (index, listName) {
+            liElem = $("<li>");
+            ulElem.append(liElem);
+            itemsList = getItems(listName);
+            numItems = keys(itemsList).length;
+            aElem = $("<a href='items.html?list="+encodeURIComponent(listName)+"'>" + escapeHTML(listName) + "<span class='ui-li-count'>" + numItems + "</span></a>");
+            liElem.append(aElem);
+            aElem.click(function () {
+                console.log("displayLists: clicked on " + value);
+                //displayItems(value);
+                return true;
+            });//aElem.click
+        });//each
 
-		//replace the current lists div contents with the new unordered list
-		$("#lists").html(ulElem);
+        //replace the current lists div contents with the new unordered list
+        $("#lists").html(ulElem);
 
-		//have to explicitly transform to listview after initial page load
-		ulElem.listview();
-	}
+        //have to explicitly transform to listview after initial page load
+        ulElem.listview();
+    }
 }//displayLists
 
 $('#choose-list-page').bind('pageinit', function(event) {
-	displayLists();
+    displayLists();
 });
