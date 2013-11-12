d7fb424a7985fbe2ace61b1b5cc2337500c1c19f
davidblumenthal <github@blumenthal.com>
2013-11-11 21:37:31 -0600

    Added initial tests with Gson

--- listey/src/com/blumenthal/listey/ListeyServlet.java
+++ listey/src/com/blumenthal/listey/ListeyServlet.java
@@ -1,17 +1,23 @@
 package com.blumenthal.listey;
 import java.io.IOException;
+import java.util.Set;
 import java.util.logging.Logger;
-import javax.servlet.http.*;
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Text;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
 
 
 
@@ -68,6 +74,13 @@ public class ListeyServlet extends HttpServlet {
         	}//if should replace
         	else {
         		passedSerializedHash = currentSerializedHash;
+        	}
+        	Gson gson = new Gson();
+        	ListeyData data = gson.fromJson(passedSerializedHash, ListeyData.class);
+        	log.info("doPost: listeyData.lastUpdate = " + data.lastUpdate);
+        	Set<String> listNames = data.lists.keySet();
+        	for(String listName : listNames ){
+        	    log.info("List Name = " + listName);
         	}
         }//if user
         else {
