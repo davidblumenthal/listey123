d769aab7edc70ff0b006c46911d7a86c93d24f1d
davidblumenthal <github@blumenthal.com>
2013-12-18 20:28:25 -0600

    Added fromJson()

--- listey/src/com/blumenthal/listey/ListeyDataMultipleUsers.java
+++ listey/src/com/blumenthal/listey/ListeyDataMultipleUsers.java
@@ -1,11 +1,10 @@
 package com.blumenthal.listey;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /** This class is a java implementation of the JSON primitive for spec version 2
  * JSON Data Format
@@ -21,5 +20,25 @@ import java.util.Set;
  */
 public class ListeyDataMultipleUsers {
 	public Long lastUpdate;
-	public Map< String, ListeyDataOneUser> userData;
-}//ListeyDataV2
+	public Map< String, ListeyDataOneUser> userData = new HashMap<String, ListeyDataOneUser>();
+	
+	/**
+	 * No-argument constructor desired by GSON
+	 */
+	public ListeyDataMultipleUsers() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Return an object from serialized JSON string
+	 */
+	public static ListeyDataMultipleUsers fromJson(String jsonString) {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.registerTypeAdapter(ListInfo.class, new ListInfoJsonAdapter());
+		gsonBuilder.registerTypeAdapter(ItemInfo.class,  new ItemInfoJsonAdapter());
+		Gson gson = gsonBuilder.create();
+		ListeyDataMultipleUsers rv = gson.fromJson(jsonString, ListeyDataMultipleUsers.class);
+		return rv;
+	}//fromJson
+}//ListeyDataMultipleUsers
