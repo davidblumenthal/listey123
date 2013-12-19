b27226dbcfa1b556d6ca23af5d93e483ec12de11
davidblumenthal <github@blumenthal.com>
2013-12-18 20:20:33 -0600

    Added toEntity() and constants

--- listey/src/com/blumenthal/listey/ItemInfo.java
+++ listey/src/com/blumenthal/listey/ItemInfo.java
@@ -6,11 +6,20 @@ package com.blumenthal.listey;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.blumenthal.listey.ListeyDataOneUser.ItemStatus;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
 
 public class ItemInfo {
 	public static final String KIND = "item";//kind in the datastore
+	public static final String NAME = "name";
+	public static final String STATUS = "status";
+	public static final String COUNT = "count";
+	public static final String LAST_UPDATE = "lastUpdate";
+	
+	public static enum ItemStatus {
+	    ACTIVE,
+	    COMPLETED
+	}
 	
 	public String name;
 	public String uniqueId;
@@ -29,10 +38,25 @@ public class ItemInfo {
 			throw new IllegalStateException("The constructor was called with an entity of the wrong kind.");
 		}//if unexpected kind
 		
-		lastUpdate = (Long) entity.getProperty("lastUpdate");
-		name = (String) entity.getProperty("displayName");
+		lastUpdate = (Long) entity.getProperty(LAST_UPDATE);
+		name = (String) entity.getProperty(NAME);
 		uniqueId = (String) entity.getKey().getName();
-		count = (Integer) entity.getProperty("count");
-		status = ItemStatus.valueOf((String) entity.getProperty("status"));
+		count = (Integer) entity.getProperty(COUNT);
+		status = ItemStatus.valueOf((String) entity.getProperty(STATUS));
 	}//ItemInfo(Entity)
+	
+	
+	
+	/**
+	 * @param parent
+	 * @return an entity that represents this object (but not its child objects)
+	 */
+	public Entity toEntity(Key parent) {
+		Entity entity = new Entity(KIND, uniqueId, parent);
+		entity.setProperty(STATUS, status.toString());
+		entity.setProperty(NAME, name);
+		entity.setProperty(COUNT,  count);
+		entity.setProperty(LAST_UPDATE, lastUpdate);
+		return entity;
+	}//toEntity
 }//ItemInfo
