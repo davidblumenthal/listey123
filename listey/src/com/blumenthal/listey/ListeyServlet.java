73e27d38924612a2ecb6a2c2a2cce18780937e06
davidblumenthal <github@blumenthal.com>
2014-02-05 20:37:15 -0600

    Added a transaction loop to prevent concurrent update collisions

--- listey/src/com/blumenthal/listey/ListeyServlet.java
+++ listey/src/com/blumenthal/listey/ListeyServlet.java
@@ -1,6 +1,7 @@
 package com.blumenthal.listey;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -12,6 +13,8 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Transaction;
+import com.google.appengine.api.datastore.TransactionOptions;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -56,26 +59,47 @@ public class ListeyServlet extends HttpServlet {
         if (user != null) {
         	log.info("doPost: User=" + user.getEmail());
 
-        	//Load the current data from datastore
-        	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        	ListeyDataMultipleUsers currentData = new ListeyDataMultipleUsers(datastore, user.getEmail());
-
         	//Parse the passed data
     		ListeyDataMultipleUsers passedData = ListeyDataMultipleUsers.fromJson(user.getEmail(), jsonString);
     		
-    		//Compare the stored and the passed data.
-    		DataStoreUniqueId uniqueIdCreator = new DataStoreUniqueId();
-    		List<Entity> updateEntities = new ArrayList<Entity>();
-    		List<Key> deleteKeys = new ArrayList<Key>();
-    		ListeyDataMultipleUsers updatedData = ListeyDataMultipleUsers.compareAndUpdate(uniqueIdCreator, currentData, passedData, updateEntities, deleteKeys);
-    		log.info("doPost: passedData = " + jsonString);
-    		log.info("        currentData= " + currentData.toJson());
-    		log.info("       updatedData = " + updatedData.toJson());
-    		log.info("doPost: updating " + updateEntities.size() + " entities, deleting " + deleteKeys.size() + " entities");
-    		datastore.put(updateEntities);
-    		datastore.delete(deleteKeys);
-    		
-    		jsonString = updatedData.toJson();
+        	//Load the current data from datastore
+        	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        	int retries=10;
+        	while (true) {
+        		TransactionOptions options = TransactionOptions.Builder.withXG(true);
+        		Transaction txn = datastore.beginTransaction(options);
+        		try {
+        			ListeyDataMultipleUsers currentData = new ListeyDataMultipleUsers(datastore, user.getEmail());
+
+        			//Compare the stored and the passed data.
+        			DataStoreUniqueId uniqueIdCreator = new DataStoreUniqueId();
+        			List<Entity> updateEntities = new ArrayList<Entity>();
+        			List<Key> deleteKeys = new ArrayList<Key>();
+        			ListeyDataMultipleUsers updatedData = ListeyDataMultipleUsers.compareAndUpdate(uniqueIdCreator, currentData, passedData, updateEntities, deleteKeys);
+        			log.info("doPost: passedData = " + jsonString);
+        			log.info("        currentData= " + currentData.toJson());
+        			log.info("       updatedData = " + updatedData.toJson());
+        			log.info("doPost: updating " + updateEntities.size() + " entities, deleting " + deleteKeys.size() + " entities");
+        			datastore.put(updateEntities);
+        			datastore.delete(deleteKeys);
+        			
+        			jsonString = updatedData.toJson();
+        			
+        	        txn.commit();
+        	        break;//out of retries loop
+        		}
+        		catch (ConcurrentModificationException e) {
+        			if (retries == 0) {
+        				throw e;
+        			}
+        			// Allow retry to occur
+        			--retries;
+        		} finally {
+        			if (txn.isActive()) {
+        				txn.rollback();
+        			}
+        		}
+        	}//while true
         }//if user
         else {
         	resp.setStatus(403);//unauthorized
